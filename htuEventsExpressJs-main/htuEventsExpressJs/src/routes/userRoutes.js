5b1f095de5417f199392049fc3279503e9745a89
AyaAbdulrahman <103676872+AyaAbdulrahman@users.noreply.github.com>
2025-03-20 22:55:29 +0300

    removed unused backend files

--- htuEventsExpressJs-main/htuEventsExpressJs/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Get user data
-router.get('/', userController.getUser);
-
-module.exports = router;
